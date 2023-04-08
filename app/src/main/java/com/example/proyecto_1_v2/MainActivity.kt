14456e3188f4e327a904af6028b26c78f7409beb
BuildTools <unconfigured@null.spigotmc.org>
2023-04-08 00:31:17 -0600

    v4

--- app/src/main/java/com/example/proyecto_1_v2/MainActivity.kt
+++ app/src/main/java/com/example/proyecto_1_v2/MainActivity.kt
@@ -3,14 +3,18 @@ package com.example.proyecto_1_v2
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
 import android.widget.ArrayAdapter
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import com.example.proyecto_1_v2.databinding.ActivityMainBinding
 import java.util.TimerTask
 
 class MainActivity : AppCompatActivity() {
     private lateinit var binding: ActivityMainBinding
-
+    private var diaSeleccionado = 0
+    private var mesSeleccionado = 0
+    private var anioSeleccionado = 0
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -36,6 +40,68 @@ class MainActivity : AppCompatActivity() {
     }
 
     fun onDateSelected(day:Int, month:Int, year:Int){
-        binding.FechaNacText.setText("$day / $month / $year")
+        diaSeleccionado = day
+        mesSeleccionado = month + 1
+        anioSeleccionado = year
+        binding.FechaNacText.setText("$day / $mesSeleccionado  / $year")
+    }
+    fun validarCorreoElectronico(correo: String): Boolean {
+        return android.util.Patterns.EMAIL_ADDRESS.matcher(correo).matches()
+    }
+    fun BEnviar(view: View) {
+        val email = binding.CorreoText.text.toString()
+        val nombre = binding.nombreText.text.toString()
+        val apellido = binding.ApellidosText.text.toString()
+        val Ncuenta = binding.NoCuentaText.text.toString()
+//        val Carrera = findViewById<Spinner>(R.id.mi_spinner)
+        var fecha  = binding.FechaNacText.text.toString()
+
+        println("Fecha de nacmiento es: $fecha ------------------------------------------------------------------------------")
+
+        if (email.isEmpty()) {
+            binding.CorreoText.error = "Ingrese un correo electrónico"
+
+        } else {
+            if(validarCorreoElectronico(email)){
+                //incresar acciones a realizar
+            }else{
+
+                binding.CorreoText.error = "Correo electrónico no valido"
+
+            }
+
+        }
+
+        if (nombre.isEmpty()) {
+            binding.nombreText.error = "Ingrese tu nombre"
+
+        }
+        if (apellido.isEmpty()) {
+            binding.ApellidosText.error = "Ingrese tus apellidos"
+
+        }
+        if (Ncuenta.isEmpty()) {
+            binding.NoCuentaText.error = "Ingrese tu N° de cuenta "
+
+        }else{
+            if(Ncuenta.length == 9){
+                //incresar acciones a realizar
+            }else{
+
+                binding.NoCuentaText.error = "N° de cuenta no valido "
+
+            }
+
+        }
+       /* if (Carrera.isEmpty()) {
+            binding..error = "Ingrese tu nombre"
+
+        }*/
+        if (fecha.isEmpty()) {
+            Toast.makeText(this, "Ingrese su fecha de nacimiento", Toast.LENGTH_SHORT).show()
+
+        }
+
+
     }
 }
